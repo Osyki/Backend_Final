988a5d29f5140c84296816fec6c5bc003b29ba70
Rubio <jon.rubio.92@gmail.com>
2022-06-21 20:36:13 -0400

    First Commit

---
+++ src/main/java/com/cscloi/card_db/entity/GamePiece.java
@@ -0,0 +1,16 @@
+package com.cscloi.card_db.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class GamePiece {
+    private String gamePiecePK;
+    private String gamePieceName;
+    private String gamePieceDesc;
+}
