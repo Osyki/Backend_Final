206a5d527cff536e452423603ec4c7b71a9c5a0d
Rubio <jon.rubio.92@gmail.com>
2022-06-21 23:00:03 -0400

    Finished CRUD on individual game pieces.

--- src/main/java/com/cscloi/card_db/dao/GamePieceDao.java
+++ src/main/java/com/cscloi/card_db/dao/GamePieceDao.java
@@ -1,4 +1,20 @@
 package com.cscloi.card_db.dao;
 
+import com.cscloi.card_db.entity.Game;
+import com.cscloi.card_db.entity.GamePiece;
+
+import java.util.List;
+import java.util.Optional;
+
 public interface GamePieceDao {
+
+    List<GamePiece> all(int limit);
+
+    GamePiece get(String id);
+
+    GamePiece save(GamePiece gamePiece);
+
+    GamePiece save(String gamePiecePK, GamePiece gamePiece);
+
+    GamePiece delete(String id);
 }
--- src/main/java/com/cscloi/card_db/entity/GamePiece.java
+++ src/main/java/com/cscloi/card_db/entity/GamePiece.java
@@ -13,4 +13,12 @@ public class GamePiece {
     private String gamePiecePK;
     private String gamePieceName;
     private String gamePieceDesc;
+    private String gamePieceFK;
+
+    public boolean isValid() {
+        if (getGamePiecePK() == null || getGamePiecePK().isEmpty()) {
+            return false;
+        }
+        return getGamePieceName() != null && !getGamePieceName().isEmpty();
+    }
 }
